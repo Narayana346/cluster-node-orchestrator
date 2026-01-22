27c13428c0d25457c3073765d60170bb50e8a3fa
Narayana Maharana <narayana.maharana@esthenos.com>
2026-01-22 15:38:01 +0530

    intiate project

---
+++ node/src/test/kotlin/com/workbuddy/node/NodeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.workbuddy.node
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NodeApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
